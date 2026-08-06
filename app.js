44c1b028ac3459a59469b0901f2f55e8d0e6cd87
Mohammad Alorfaa <mohammadalorfaa2033@gmail.com>
2026-08-06 13:54:32 +0300

    Update index.html file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ if (typeof XLSX === 'undefined') {
 }
 
 // 🌐 الرابط السحابي المباشر لملف غوغل شيت (تأكد من استخدام خيار النشر للويب بصيغة CSV)
-const googleSheetCsvUrl = "⚠️ ضع_هنا_رابط_الـ_CSV_الذي_نسخته_من_خطوة_النشر ⚠️";
+const googleSheetCsvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrN_bE_7vmgWHq0U2Aw55WcI8TsFgPPTHre3QMexHf2oWaYhZdAaXU2o6c5GXKEtFRKlMVk_dIdvI_/pub?gid=0&single=true&output=csv";
 
 var staffDb = [];
 var groupCounter = 0; 
